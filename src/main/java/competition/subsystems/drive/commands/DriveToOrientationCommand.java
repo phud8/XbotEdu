6357d29039a338cc0ca64fb91e1d08aed6ee8d90
Phu <phudoan923@icloud.com>
2024-08-10 13:11:50 -0700

    Rotation
    rotate 90 degrees done
    working on go to orientation

--- src/main/java/competition/subsystems/drive/commands/DriveToOrientationCommand.java
+++ src/main/java/competition/subsystems/drive/commands/DriveToOrientationCommand.java
@@ -2,21 +2,34 @@ package competition.subsystems.drive.commands;
 
 import javax.inject.Inject;
 
+import competition.subsystems.pose.PoseSubsystem;
 import xbot.common.command.BaseCommand;
 import competition.subsystems.drive.DriveSubsystem;
 
 public class DriveToOrientationCommand extends BaseCommand {
 
     DriveSubsystem drive;
+    PoseSubsystem pose;
+
 
     @Inject
     public DriveToOrientationCommand(DriveSubsystem driveSubsystem) {
         this.drive = driveSubsystem;
     }
-
+    public double targetHeading;
+    double currentRotation;
+    double rotationChange;
+    double oldRotation;
     public void setTargetHeading(double heading) {
         // This method will be called by the test, and will give you a goal heading.
         // You'll need to remember this target position and use it in your calculations.
+
+        if(targetHeading<0)
+        {
+            targetHeading=180+(180+heading);
+        }
+        else
+            targetHeading=heading;
     }
 
     @Override
@@ -33,8 +46,28 @@ public class DriveToOrientationCommand extends BaseCommand {
 
         // How you do this is up to you. If you get stuck, ask a mentor or student for
         // some hints!
-    }
+        currentRotation=pose.getCurrentHeading().getDegrees();
+        if(currentRotation<0)
+        {
+            currentRotation=180+(180+pose.getCurrentHeading().getDegrees());
+        }
+        double error=targetHeading-currentRotation;
+        rotationChange=currentRotation-oldRotation;
+        double power=(.01*error)-(.5*rotationChange);
 
+    if(currentRotation<targetHeading-20||currentRotation>targetHeading+20)
+    {
+        drive.arcadeDrive(-1,0);
+    }
+       else if(currentRotation>targetHeading+1||currentRotation<targetHeading-1)
+    {
+
+        drive.arcadeDrive(-power,0);
+    }
+        else
+                drive.arcadeDrive(0,0);
+    oldRotation=currentRotation;
+    }
     @Override
     public boolean isFinished() {
         // Modify this to return true once you have met your goal,
--- src/main/java/competition/subsystems/drive/commands/TurnLeft90DegreesCommand.java
+++ src/main/java/competition/subsystems/drive/commands/TurnLeft90DegreesCommand.java
@@ -18,27 +18,49 @@ public class TurnLeft90DegreesCommand extends BaseCommand {
     }
 public double startRotation;
     public double currentRotation;
+    public double targetRotation;
+    public double oldRotation;
+    public double rotationChange;
     @Override
     public void initialize() {
          startRotation=pose.getCurrentHeading().getDegrees();
-         currentRotation=pose.getCurrentHeading().getDegrees();
+
+        if(startRotation<0)
+        {
+            startRotation=180+(180+pose.getCurrentHeading().getDegrees());
+        }
+        else
+        startRotation=pose.getCurrentHeading().getDegrees();
         System.out.println(startRotation);
+        targetRotation=startRotation+90;
+
+        System.out.println(targetRotation);
+
     }
 
     @Override
     public void execute() {
-
         currentRotation=pose.getCurrentHeading().getDegrees();
-        if(currentRotation<startRotation+90)
+        if(currentRotation<0)
+        {
+            currentRotation=180+(180+pose.getCurrentHeading().getDegrees());
+        }
+        double error=targetRotation-currentRotation;
+        rotationChange=currentRotation-oldRotation;
+        double power=(.01*error)-(.5*rotationChange);
+if(currentRotation<targetRotation-20||currentRotation>targetRotation+20)
+{
+    drive.arcadeDrive(-1,0);
+}
+       else if(currentRotation>targetRotation+1||currentRotation<targetRotation-1)
         {
 
-            drive.arcadeDrive(-1,0);
+            drive.arcadeDrive(-power,0);
         }
         else
             drive.arcadeDrive(0,0);
+oldRotation=currentRotation;
 
-
-        System.out.println(currentRotation);
     }
 
 }
